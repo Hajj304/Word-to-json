9ed7c1aa6587069389a54d41ca4935aa8e4918cd
bachir salhi <designama23@gmail.com>
2025-07-08 00:27:53 +0100

    Create Exemple_JSON.py
    Exemple de sortie JSON

---
+++ Exemple_JSON.py
@@ -0,0 +1,16 @@
+[
+  {
+    "title": "Introduction",
+    "content": [
+      "Ce document présente les grandes lignes de la réforme...",
+      "Il est basé sur le michtaq de 1999..."
+    ]
+  },
+  {
+    "title": "Objectifs",
+    "content": [
+      "Assurer l’égalité des chances.",
+      "Adapter l’enseignement aux besoins de la société."
+    ]
+  }
+]
